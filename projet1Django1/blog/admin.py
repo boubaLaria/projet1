4a0a4492a91c36c90b800d74770b07f2c7ea7327
Boubacar Diallo <ladialloboubacar@gmail.com>
2020-12-08 16:58:41 +0000

    mise en place du model

--- projet1Django1/blog/admin.py
+++ projet1Django1/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+admin.site.register(Client)
+admin.site.register(Commande)
+admin.site.register(Plat)
+admin.site.register(Restaurant)
+admin.site.register(Repas)
+admin.site.register(List_plat)
