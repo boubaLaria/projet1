ad1e3a30089fa03814cfe68696745673a9f98a3c
Boubacar Diallo <ladialloboubacar@gmail.com>
2020-12-11 11:52:30 +0000

    amelioration des models

--- projet1Django1/blog/models.py
+++ projet1Django1/blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django import forms
 from phone_field import PhoneField
+from django.core.validators import RegexValidator, EmailValidator
 
-
+"""
+    Creation du model client pour la table client
+"""
 class Client(models.Model):
     GENRE=(
         ('M','Masculin'),
@@ -11,7 +14,15 @@ class Client(models.Model):
     nom_client=models.CharField(max_length=50)
     prenom_client=models.CharField(max_length=100)
     genre=models.CharField(max_length=1, choices=GENRE)
-    telephone=PhoneField(blank=True, help_text='Contact phone number')
+    phone_regex = RegexValidator(
+        regex=r'^\d{9}$',
+        message="Le numero doit contenir 9 chiffres comme: 622678756"
+    )
+    telephone = models.CharField(
+        validators=[phone_regex],
+        verbose_name=("Mobile phone"),
+        max_length=17, blank=True, null=True
+    )
     email=models.EmailField(max_length=100, unique=True)
     password=models.CharField(max_length=30)
     adresse=models.CharField(max_length=50)
@@ -24,8 +35,11 @@ class Plat(models.Model):
     prix=models.IntegerField()
     type_plat=models.CharField(max_length=100)
     description=models.CharField(max_length=200)
-    photo=models.CharField(max_length=12)
-
+    photo = models.ImageField(
+        verbose_name=("Photo"), 
+        upload_to='photos/', 
+        default='photos/assiette.png'
+    )
     def __str__(self):
         return f"{self.nom_plat}, {self.prix}, {self.type_plat}, {self.description}"
 
@@ -39,7 +53,7 @@ class Restaurant(models.Model):
 
 class Commande(models.Model):
     STATUS=(('Livre','Livré'),('Attente','En Attente'))
-    date_com=models.DateField('date published')
+    date_com=models.DateField('Date')
     status=models.CharField(max_length=20,choices=STATUS)
     plat=models.ManyToManyField(Plat)
     id_client=models.ForeignKey(Client, on_delete=models.CASCADE)
